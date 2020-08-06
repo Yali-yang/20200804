8b1f75fa0552b6e89fa57fc5aa6175876c5b3bae
penyang <penyang@aijiatui.com>
2020-08-06 15:28:44 +0800

    0806

--- src/main/java/com/example/yali/YaliApplication.java
+++ src/main/java/com/example/yali/YaliApplication.java
@@ -2,9 +2,13 @@ package com.example.yali;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class YaliApplication {
+    // 这是0806的分支
 
     public static void main(String[] args) {
         SpringApplication.run(YaliApplication.class, args);
